1fdf4089bf657c4aeafd205f76a8dfdb404ad366
hossam ramadan <hossamramadan115@gmail.com>
2025-02-20 15:20:38 +0200

    create custom appbar and custom container

---
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'custom_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 24, right: 24, top: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          CustomIcon(
+            icon: FontAwesomeIcons.magnifyingGlass,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/home/presentation/views/widgets/custom_icon.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key, required this.icon, required this.onPressed});
+  final IconData? icon;
+  final Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      color: Colors.white,
+      onPressed: onPressed,
+      icon: Icon(icon),
+    );
+  }
+}
