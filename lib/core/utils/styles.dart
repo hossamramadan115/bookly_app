bc0ec61aa7dc496eba759b6e43c286d6b5184b07
hossam ramadan <hossamramadan115@gmail.com>
2025-02-22 16:44:33 +0200

    add styles part2

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -5,4 +5,20 @@ abstract class Styles {
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
+  static const textstyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const textstyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+  static const textstyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textstyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
 }
