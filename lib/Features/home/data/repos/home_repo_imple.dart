ff4cc6d7c6476e1649798d4180b7c15330a34d5b
hossam ramadan <hossamramadan115@gmail.com>
2025-02-28 16:18:13 +0200

    create api service

---
+++ lib/Features/home/data/repos/home_repo_imple.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failuer.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImple implements HomeRepo{
+  @override
+  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failuer, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+  
+}
